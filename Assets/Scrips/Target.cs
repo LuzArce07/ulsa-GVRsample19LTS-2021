10890656835b49301d2aa60d118a69874e9f2469
luzarce <luzarce7@gmail.com>
2021-04-14 18:41:07 -0700

    DialogItem

---
+++ Assets/Scrips/Target.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+  [SerializeField]
+  Color catchColor = Color.cyan;
+
+  Renderer render;
+  [SerializeField]
+  MessageController textInteraction;
+  [SerializeField]
+  DialogItem dialogItem;
+
+  List<DialogItem> dialogsItems;
+
+  void Awake()
+  {
+    render = GetComponent<Renderer>();
+  }
+
+  public void HandleColor() => render.material.color = catchColor;
+
+  public void HandleTextInteraction() =>  textInteraction.gameObject?.SetActive(!textInteraction.gameObject.activeSelf);
+
+  public void handleClick()
+  {
+    HandleColor();
+    HandleTextInteraction();
+    textInteraction.Message = dialogItem.Message;
+  }
+}
